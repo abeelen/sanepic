e216604474337b20ea293f427cb97ecc81f1daa7
Mathieu Hhusson <mathieu.husson@ias.u-psud.fr>
2009-08-25 09:04:46 +0000

    changed delete frnum position
    git-svn-id: https://svn.ias.u-psud.fr/sanepic/trunk@137 339b86bc-bb6a-0410-a225-86439261ec80

--- sanePos/src/MainSanepicCorr_mpi.cpp
+++ sanePos/src/MainSanepicCorr_mpi.cpp
@@ -353,7 +353,7 @@ int main(int argc, char *argv[])
 		//delete [] extentnoiseSp_allorder;
 
 		delete [] ruleorder;
-		delete [] frnum;
+
 
 	}
 
@@ -365,6 +365,7 @@ int main(int argc, char *argv[])
 	iframe_max = frnum[rank+1];
 	rank_det = 0;
 	size_det = 1;
+	delete [] frnum;
 
 #else
 	iframe_min = 0;
