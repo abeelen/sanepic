302a581910b7851727cf7e84a6de47d07fe41852
Mathieu Hhusson <mathieu.husson@ias.u-psud.fr>
2009-08-26 15:32:59 +0000

    modified frnum gestion
    git-svn-id: https://svn.ias.u-psud.fr/sanepic/trunk@148 339b86bc-bb6a-0410-a225-86439261ec80

--- sanePos/src/MainSanepicCorr_mpi.cpp
+++ sanePos/src/MainSanepicCorr_mpi.cpp
@@ -318,6 +318,9 @@ int main(int argc, char *argv[])
 	/********************* Define parallelization scheme   *******/
 
 	long *frnum ;
+	if(rank>0)
+
+
 
 	if (rank == 0){
 
@@ -355,6 +358,8 @@ int main(int argc, char *argv[])
 		delete [] ruleorder;
 
 
+	}else{
+		frnum = new long[ntotscan+1];
 	}
 
 	MPI_Bcast(nsamples,ntotscan,MPI_LONG,0,MPI_COMM_WORLD);
