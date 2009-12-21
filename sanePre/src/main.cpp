838f56df91c72ce1f637379c6880e1aef6c52743
Mathieu Hhusson <mathieu.husson@ias.u-psud.fr>
2009-12-21 14:25:09 +0000

    - added binmap_noisevar and _invnoisevaruncpix in sanePre instead of sanePic
    git-svn-id: https://svn.ias.u-psud.fr/sanepic/trunk@300 339b86bc-bb6a-0410-a225-86439261ec80

--- sanePre/src/main.cpp
+++ sanePre/src/main.cpp
@@ -696,6 +696,49 @@ int main(int argc, char *argv[])
 		fnaivname = '!' + dir.outdir + "hits.fits";
 		write_fits_wcs(fnaivname, wcs, NAXIS1, NAXIS2, 'd', (void *)map1d);
 
+
+		for (long ii=0; ii<NAXIS1; ii++) {
+			for (long jj=0; jj<NAXIS2; jj++) {
+				mi = jj*NAXIS1 + ii;
+				if (indpix[mi] >= 0){
+					map1d[mi] = Mptot[indpix[mi]];
+				} else {
+					map1d[mi] = NAN;
+				}
+			}
+		}
+
+
+		fnaivname = '!' + dir.outdir + "binMap_noisevar.fits"; // write preconditioner
+		//					write_fits(fname, pixdeg, NAXIS1, NAXIS2, tancoord, tanpix, coordsyst, 'd', (void *)map1d);
+		write_fits_wcs(fnaivname, wcs, NAXIS1, NAXIS2, 'd', (void *)map1d);
+
+		for (long ii=0; ii<NAXIS1; ii++) {
+			for (long jj=0; jj<NAXIS2; jj++) {
+				mi = jj*NAXIS1 + ii;
+				map1d[mi] = 0.0;
+			}
+		}
+
+		//TODO : Treat the 2 pixels properly
+		if (addnpix){
+			for (long iframe = 0;iframe<samples_struct.ntotscan;iframe++){
+				for (long ii=0; ii<NAXIS1; ii++) {
+					for (long jj=0; jj<NAXIS2; jj++) {
+						mi = jj*NAXIS1 + ii;
+						long long ll = factdupl*NAXIS1*NAXIS2 + iframe*npixsrc + indpsrc[mi];
+						if ((indpsrc[mi] != -1) && (indpix[ll] != -1))
+							map1d[mi] += 1.0/Mptot[indpix[ll]];
+					}
+				}
+			}
+
+			fnaivname = '!' + dir.outdir + "optimMap_" + "_invnoisevaruncpix.fits";
+			//						write_fits(fname, pixdeg, NAXIS1, NAXIS2, tancoord, tanpix, coordsyst, 'd', (void *)map1d);
+			write_fits_wcs(fnaivname, wcs, NAXIS1, NAXIS2, 'd', (void *)map1d);
+		}
+
+
 		delete [] map1d;
 		printf("End of saneNaiv\n");
 
