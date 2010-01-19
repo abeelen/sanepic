da24fcdfa274c0f25c60db660dacb4dd9a08344c
Mathieu Hhusson <mathieu.husson@ias.u-psud.fr>
2010-01-19 16:36:29 +0000

    - modifications for PARA_BOLO Nocorr
    git-svn-id: https://svn.ias.u-psud.fr/sanepic/trunk@335 339b86bc-bb6a-0410-a225-86439261ec80

--- saneLib/src/NoCorr_preprocess.cpp
+++ saneLib/src/NoCorr_preprocess.cpp
@@ -23,7 +23,7 @@ using namespace std;
 void do_PtNd_nocorr(double *PNd,string tmp_dir, struct param_process proc_param, struct param_positions pos_param,
 		struct samples samples_struct, struct detectors det, double f_lppix, double f_lppix_Nk,
 		long addnpix, long ns, long long *indpix, long long *indpsrc, long NAXIS1, long NAXIS2, long long npix,
-		long long npixsrc, long iframe, double *S)
+		long long npixsrc, long iframe, double *S, int rank, int size)
 {
 
 
@@ -35,7 +35,7 @@ void do_PtNd_nocorr(double *PNd,string tmp_dir, struct param_process proc_param,
 
 	long long *samptopix;
 	double *bfilter, *Nk, *data, *data_lp, *Ps;
-	short *flag, *flpoint;
+	short *flag;
 	double powered;
 
 	samptopix = new long long[ns];
@@ -43,9 +43,7 @@ void do_PtNd_nocorr(double *PNd,string tmp_dir, struct param_process proc_param,
 	Nk = new double[ns/2+1];
 
 
-//	data =  new double[ns];
 	data_lp = new double[ns];
-	flpoint = new short[ns];
 	Ps = new double[ns];
 
 	int factdupl = 1;
@@ -56,188 +54,181 @@ void do_PtNd_nocorr(double *PNd,string tmp_dir, struct param_process proc_param,
 
 	fits_filename = samples_struct.fits_table[iframe];
 
-
-	//for (long idet=rank*ndet/size;idet<(rank+1)*ndet/size;idet++){
-	for (long idet=0;idet<det.ndet;idet++){
-		field = det.boloname[idet];
-
-
-
-		if (S != NULL){
-			read_flpoint_from_fits(fits_filename, flpoint);
-		}
-
-
-		long test_ns;
-		read_signal_from_fits(fits_filename, field, data, test_ns);
-		if (test_ns != ns) {
-			cerr << "Read signal does not correspond to frame size : Check !!" << endl;
-			exit(-1);
-		}
-
-		read_flag_from_fits(fits_filename , field, flag, test_ns);
-		if (test_ns != ns) {
-			cerr << "Read flag does not correspond to frame size : Check !!" << endl;
-			exit(-1);
-		}
+#ifdef PARA_BOLO
+	for (long idet=rank*det.ndet/size;idet<(rank+1)*det.ndet/size;idet++){
+#else
+		for (long idet=0;idet<det.ndet;idet++){
+#endif
+			field = det.boloname[idet];
 
 
 
-
-		//// Read pointing
-		read_samptopix(ns, samptopix, tmp_dir, idet, iframe,det.boloname);
-
-
-		if (S != NULL){
-
-			if (addnpix){
-				deproject(S,indpix,samptopix,ns,NAXIS1, NAXIS2,npix,Ps,2,factdupl,samples_struct.ntotscan,indpsrc,npixsrc);
-			} else {
-				deproject(S,indpix,samptopix,ns,NAXIS1, NAXIS2,npix,Ps,2,factdupl);
+			long test_ns;
+			read_signal_from_fits(fits_filename, field, data, test_ns);
+			if (test_ns != ns) {
+				cerr << "Read signal does not correspond to frame size : Check !!" << endl;
+				exit(-1);
 			}
 
-		}
-
-
-		if (S != NULL){
-			//********************  pre-processing of data ********************//
-			MapMakPreProcessData(data,flag,ns,proc_param.napod,proc_param.poly_order,f_lppix,data_lp,bfilter,
-					proc_param.NORMLIN,proc_param.NOFILLGAP,proc_param.remove_polynomia,Ps);
-		}
-		else {
-			MapMakPreProcessData(data,flag,ns,proc_param.napod,proc_param.poly_order,f_lppix,data_lp,bfilter,
-					proc_param.NORMLIN,proc_param.NOFILLGAP,proc_param.remove_polynomia);
-		}
-
-
-		/************************************************************************************/
-
-		// end of preprocess begin of fdata
-
-		for (long ii=0;ii<ns/2+1;ii++){
-			powered=pow(double(ii)/f_lppix, 16);
-			bfilter[ii] = powered /(1.0+powered);
-		}
-
-
-
-		//****************** Compute (or read) input power spectrum of the NOISE  ***************//
-		extentnoiseSp = samples_struct.noise_table[iframe];
-		nameSpfile = tmp_dir + field + extentnoiseSp;
-
-		readNSpectrum(nameSpfile,bfilter,ns,proc_param.fsamp,Nk);
-
-
-		//********************** compute P^t N-1 d ************************//
-		compute_PtNmd(data_lp,Nk,ns,NAXIS1, NAXIS2,indpix,samptopix,npix,PNd);
-
-
-	}// end of idet loop
-
-
-	delete[] samptopix;
-	delete[] bfilter;
-	delete[] Nk;
-
-	delete[] data;
-	delete[] data_lp;
-
-	delete[] flag;
-	delete[] flpoint;
-
-	delete[] Ps;
-
-
-}
-
-
-
-
-
-//void do_PtNPS_nocorr(double *S, string *extentnoiseSp_all, string noiseSppreffile, string dir,
-//		/* string termin, */ string dirfile, std::vector<string> bolonames, double f_lppix,
-//		double fsamp, bool flgdupl, int factdupl, long ns,
-//		long ndet, long long *indpix, long NAXIS1, long NAXIS2, long long npix,
-//		long iframe, double *PtNPmatS, double *Mp, long *hits)
-
-void do_PtNPS_nocorr(double *S, string *extentnoiseSp_all, struct directories dir,
-		struct detectors det,double f_lppix,double fsamp, bool flgdupl, long ns,
-		long long *indpix, long NAXIS1, long NAXIS2, long long npix,
-		long iframe, double *PtNPmatS, double *Mp, long *hits)
-
-{
-
-	string field;
-	string extentnoiseSp;
-	string nameSpfile;
-
-
-	long long *samptopix;
-	double *bfilter, *Nk, *Ps;
-	double powered;
-
-	samptopix = new long long[ns];
-	bfilter = new double[ns/2+1];
-	Nk = new double[ns/2+1];
-	Ps = new double[ns];
-
-
-	int factdupl = 1;
-	if(flgdupl==1) factdupl = 2;
-
-
-	//for (long idet=rank*ndet/size;idet<(rank+1)*ndet/size;idet++){
-	for (long idet=0;idet<det.ndet;idet++){
-		field = det.boloname[idet];
-
-		read_samptopix(ns, samptopix, dir.tmp_dir, idet, iframe,det.boloname);
-
-
-		// AS
-		deproject(S,indpix,samptopix,ns,NAXIS1, NAXIS2,npix,Ps,flgdupl,factdupl);
-
-
-
-
-		for (long ii=0;ii<(ns)/2+1;ii++){
-			powered=pow(double(ii)/f_lppix, 16);
-			bfilter[ii] = powered /(1.0+powered);
-		}
-
-		extentnoiseSp = extentnoiseSp_all[iframe];
-		//sprintf(nameSpfile,"%s%s%s",noiseSppreffile.c_str(),field.c_str(),extentnoiseSp.c_str());
-		nameSpfile = dir.tmp_dir + field + extentnoiseSp;
-		readNSpectrum(nameSpfile,bfilter,ns,fsamp,Nk);
-
-
-		//AtN-1A AS (espensive part)
-		compute_PtNmd(Ps,Nk,ns,NAXIS1, NAXIS2,indpix,samptopix,npix,PtNPmatS);
-
-
-		//Compute weight map for preconditioner
-		if ((Mp != NULL))
-			compute_diagPtNP(Nk,samptopix,ns,NAXIS1, NAXIS2,indpix,npix,f_lppix,Mp);
-
-
-		//compute hit counts
-		if (hits != NULL){
-			for (long ii=0;ii<ns;ii++){
-				hits[indpix[samptopix[ii]]] += 1;
+			read_flag_from_fits(fits_filename , field, flag, test_ns);
+			if (test_ns != ns) {
+				cerr << "Read flag does not correspond to frame size : Check !!" << endl;
+				exit(-1);
 			}
+
+
+
+
+			//// Read pointing
+			read_samptopix(ns, samptopix, tmp_dir, idet, iframe,det.boloname);
+
+
+			if (S != NULL){
+
+				if (addnpix){
+					deproject(S,indpix,samptopix,ns,NAXIS1, NAXIS2,npix,Ps,2,factdupl,samples_struct.ntotscan,indpsrc,npixsrc);
+				} else {
+					deproject(S,indpix,samptopix,ns,NAXIS1, NAXIS2,npix,Ps,2,factdupl);
+				}
+
+			}
+
+
+			if (S != NULL){
+				//********************  pre-processing of data ********************//
+				MapMakPreProcessData(data,flag,ns,proc_param.napod,proc_param.poly_order,f_lppix,data_lp,bfilter,
+						proc_param.NORMLIN,proc_param.NOFILLGAP,proc_param.remove_polynomia,Ps);
+			}
+			else {
+				MapMakPreProcessData(data,flag,ns,proc_param.napod,proc_param.poly_order,f_lppix,data_lp,bfilter,
+						proc_param.NORMLIN,proc_param.NOFILLGAP,proc_param.remove_polynomia);
+			}
+
+
+			/************************************************************************************/
+
+			// end of preprocess begin of fdata
+
+			for (long ii=0;ii<ns/2+1;ii++){
+				powered=pow(double(ii)/f_lppix, 16);
+				bfilter[ii] = powered /(1.0+powered);
+			}
+
+
+
+			//****************** Compute (or read) input power spectrum of the NOISE  ***************//
+			extentnoiseSp = samples_struct.noise_table[iframe];
+			nameSpfile = tmp_dir + field + extentnoiseSp;
+
+			readNSpectrum(nameSpfile,bfilter,ns,proc_param.fsamp,Nk);
+
+
+			//********************** compute P^t N-1 d ************************//
+			compute_PtNmd(data_lp,Nk,ns,NAXIS1, NAXIS2,indpix,samptopix,npix,PNd);
+
+
+		}// end of idet loop
+
+
+		delete[] samptopix;
+		delete[] bfilter;
+		delete[] Nk;
+
+		delete[] data;
+		delete[] data_lp;
+
+		delete[] flag;
+
+		delete[] Ps;
+
+
+	}
+
+
+
+
+
+
+	void do_PtNPS_nocorr(double *S, string *extentnoiseSp_all, struct directories dir,
+			struct detectors det,double f_lppix,double fsamp, bool flgdupl, long ns,
+			long long *indpix, long NAXIS1, long NAXIS2, long long npix,
+			long iframe, double *PtNPmatS, double *Mp, long *hits, int rank, int size)
+
+	{
+
+		string field;
+		string extentnoiseSp;
+		string nameSpfile;
+
+
+		long long *samptopix;
+		double *bfilter, *Nk, *Ps;
+		double powered;
+
+		samptopix = new long long[ns];
+		bfilter = new double[ns/2+1];
+		Nk = new double[ns/2+1];
+		Ps = new double[ns];
+
+
+		int factdupl = 1;
+		if(flgdupl==1) factdupl = 2;
+
+#ifdef PARA_BOLO
+		for (long idet=rank*det.ndet/size;idet<(rank+1)*det.ndet/size;idet++){
+#else
+			for (long idet=0;idet<det.ndet;idet++){
+#endif
+				field = det.boloname[idet];
+
+				read_samptopix(ns, samptopix, dir.tmp_dir, idet, iframe,det.boloname);
+
+
+				// AS
+				deproject(S,indpix,samptopix,ns,NAXIS1, NAXIS2,npix,Ps,flgdupl,factdupl);
+
+
+
+
+				for (long ii=0;ii<(ns)/2+1;ii++){
+					powered=pow(double(ii)/f_lppix, 16);
+					bfilter[ii] = powered /(1.0+powered);
+				}
+
+				extentnoiseSp = extentnoiseSp_all[iframe];
+				//sprintf(nameSpfile,"%s%s%s",noiseSppreffile.c_str(),field.c_str(),extentnoiseSp.c_str());
+				nameSpfile = dir.tmp_dir + field + extentnoiseSp;
+				readNSpectrum(nameSpfile,bfilter,ns,fsamp,Nk);
+
+
+				//AtN-1A AS (espensive part)
+				compute_PtNmd(Ps,Nk,ns,NAXIS1, NAXIS2,indpix,samptopix,npix,PtNPmatS);
+
+
+				//Compute weight map for preconditioner
+				if ((Mp != NULL))
+					compute_diagPtNP(Nk,samptopix,ns,NAXIS1, NAXIS2,indpix,npix,f_lppix,Mp);
+
+
+				//compute hit counts
+				if (hits != NULL){
+					for (long ii=0;ii<ns;ii++){
+						hits[indpix[samptopix[ii]]] += 1;
+					}
+				}
+
+
+			} // end of idet loop
+
+
+
+			delete[] samptopix;
+			delete[] bfilter;
+			delete[] Nk;
+			delete[] Ps;
+
+
 		}
-
-
-	} // end of idet loop
-
-
-
-	delete[] samptopix;
-	delete[] bfilter;
-	delete[] Nk;
-	delete[] Ps;
-
-
-}
 
 
 
--- saneLib/src/NoCorr_preprocess.h
+++ saneLib/src/NoCorr_preprocess.h
@@ -16,12 +16,12 @@
 void do_PtNd_nocorr(double *PNd,std::string tmp_dir, struct param_process proc_param, struct param_positions pos_param,
 		struct samples samples_struct, struct detectors det, double f_lppix, double f_lppix_Nk,
 		long addnpix, long ns, long long *indpix, long long *indpsrc, long NAXIS1, long NAXIS2, long long npix,
-		long long npixsrc, long iframe, double *S);
+		long long npixsrc, long iframe, double *S, int rank, int size);
 
 
 void do_PtNPS_nocorr(double *S, std::string *extentnoiseSp_all, struct directories dir,
 		struct detectors det,double f_lppix,double fsamp, bool flgdupl, long ns,
 		long long *indpix, long NAXIS1, long NAXIS2, long long npix,
-		long iframe, double *PtNPmatS, double *Mp, long *hits);
+		long iframe, double *PtNPmatS, double *Mp, long *hits, int rank, int size);
 
 #endif /* NOCORR_PREPROCESS_H_ */
