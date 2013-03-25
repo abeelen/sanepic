d70504b2d91e077133b0314e6eccdfb20decebf1
Alexandre Beelen <alexandre.beelen@ias.u-psud.fr>
2013-03-25 11:51:43 +0000

    cleaning
    git-svn-id: https://svn.ias.u-psud.fr/sanepic/trunk@1410 339b86bc-bb6a-0410-a225-86439261ec80

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(sanepic):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="sanepic" command="/usr/bin/sanepic"
--- debian/sanepic.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the sanepic package
-#
-0 4	* * *	root	[ -x /usr/bin/sanepic_maintenance ] && /usr/bin/sanepic_maintenance
